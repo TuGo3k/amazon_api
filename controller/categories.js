7da1fc80c116d379e9d9a31f6b06682d30c0491c
TuGo3k <ATuru0106@gmail.com>
2024-12-10 18:45:46 +0800

    update

--- controller/categories.js
+++ controller/categories.js
@@ -1,79 +1,60 @@
 const Category = require("../models/Category");
 const myError = require("../utils/myError");
+// const asyncHandler = require("../middleware/asyncHandler");
+const asyncHandler = require("express-async-handler");
+exports.getCategories = asyncHandler(async (req, res, next) => {
+  const categories = await Category.find();
 
-exports.getCategories = async (req, res, next) => {
-  try {
-    const categories = await Category.find();
+  res.status(200).json({
+    succes: true,
+    data: categories,
+  });
+});
+exports.getCategory = asyncHandler(async (req, res, next) => {
+  const category = await Category.findById(req.params.id);
 
-    res.status(200).json({
-      succes: true,
-      data: categories,
-    });
-  } catch (err) {
-    next(err);
+  if (!category) {
+    throw new myError(req.params.id + " ID-тэй категори байхгүй.", 403);
   }
-};
-exports.getCategory = async (req, res, next) => {
-  try {
-    const category = await Category.findById(req.params.id);
-
-    if (!category) {
-      throw new myError(req.params.id + " ID-тэй категори байхгүй.", 400)
-    }
-    res.status(200).json({
-      success: true,
-      data: category,
-    });
-  } catch (err) {
-  next(err);
-  }
-};
-exports.createCategory = async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: category,
+  });
+});
+exports.createCategory = asyncHandler(async (req, res, next) => {
   console.log("data: ", req.body);
 
-  try {
-    const category = await Category.create(req.body);
+  const category = await Category.create(req.body);
 
-    res.status(200).json({
-      succes: true,
-      data: category,
-    });
-  } catch (err) {
-   next(err);
+  res.status(200).json({
+    succes: true,
+    data: category,
+  });
+});
+exports.updateCategory = asyncHandler(async (req, res, next) => {
+  const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!category) {
+    throw new myError(req.params.id + " ID-тэй категори байхгүй.", 400);
   }
-};
-exports.updateCategory = async(req, res, next) => {
-  try {
-    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
 
-    if (!category) {
-      throw new myError(req.params.id + " ID-тэй категори байхгүй.", 400)
-    }
+  res.status(200).json({
+    success: true,
+    data: category,
+  });
+});
+exports.deleteCategory = asyncHandler(async (req, res, next) => {
+  const category = await Category.findByIdAndDelete(req.params.id);
 
-    res.status(200).json({
-      success: true,
-      data: category,
-    });
-  } catch (err) {
-    next(err);
+  if (!category) {
+    throw new myError(req.params.id + " ID-тэй категори байхгүй.", 400);
   }
-};
-exports.deleteCategory = async (req, res, next) => {
-  try {
-    const category = await Category.findByIdAndDelete(req.params.id);
 
-    if (!category) {
-      throw new myError(req.params.id + " ID-тэй категори байхгүй.", 400)
-    }
-
-    res.status(200).json({
-      success: true,
-      data: category,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: category,
+  });
+});
